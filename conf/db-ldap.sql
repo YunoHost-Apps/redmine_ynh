2ff3ad1a3f6aabe9044e46aeea794305e163fb0f
François DORIN <francois@custom-dev.fr>
2023-04-08 16:58:08 +0200

    Adding default LDAP configuration

---
+++ conf/db-ldap.sql
@@ -0,0 +1,13 @@
+INSERT INTO  public.auth_sources(type, name, host, port, base_dn, attr_login, attr_firstname, attr_lastname, attr_mail, onthefly_register, filter) VALUES (
+	'AuthSourceLdap',
+        'yunohost',
+        'localhost',
+        389,
+        'ou=users,dc=yunohost,dc=org',
+        'uid',
+        'sn',
+        'givenName',
+        'mail',
+        t,
+        '(&(objectclass=posixAccount)(permission=cn=redmine.main,ou=permission,dc=yunohost,dc=org))'
+)
