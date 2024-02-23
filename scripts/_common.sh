3ba539315ee1165b643e6c09f30f063cc3453db6
Félix Piédallu <felix@piedallu.me>
2024-02-23 19:21:44 +0100

    Install ruby as user

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,6 +10,17 @@ ruby_version=3.1
 # PERSONAL HELPERS
 #=================================================
 
+_redmine_ruby_install() {
+    pushd "$install_dir"
+    ynh_use_ruby
+    ynh_exec_as "$app" "$ynh_ruby_load_path" "$ynh_gem" update --system --no-document
+    ynh_exec_as "$app" "$ynh_ruby_load_path" "$ynh_gem" install bundler passenger --no-document
+    ynh_exec_as "$app" "$ynh_ruby_load_path" bundle config set --local without 'development test rmagick'
+    ynh_exec_as "$app" "$ynh_ruby_load_path" bundle add 'webrick' --version '~> 1.7'
+    ynh_exec_as "$app" "$ynh_ruby_load_path" bundle install
+    popd
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
