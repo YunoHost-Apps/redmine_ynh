3e1fd48ec9770f13a5df0d99c09911408e27a997
Félix Piédallu <felix@piedallu.me>
2024-02-23 22:33:52 +0100

    use ynh_gem variabel

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,12 +12,12 @@ ruby_version=3.1
 
 _redmine_ruby_install() {
     pushd "$install_dir"
-	ynh_use_ruby
-	ynh_gem update --system --no-document
-	ynh_gem install bundler passenger --no-document
-	bundle config set --local without 'development test rmagick'
-	bundle add 'webrick' --version '~> 1.7'
-	bundle install
+    ynh_use_ruby
+    "$ynh_gem" update --system --no-document
+    "$ynh_gem" install bundler passenger --no-document
+    bundle config set --local without 'development test rmagick'
+    bundle add 'webrick' --version '~> 1.7'
+    bundle install
     popd
 }
 
