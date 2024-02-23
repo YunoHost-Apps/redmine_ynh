f38f407d8d866799cef6067d1d9d74169f9e38cc
Félix Piédallu <felix@piedallu.me>
2024-02-23 20:18:42 +0100

    run gem istall as root

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,7 +14,7 @@ _redmine_ruby_install() {
     pushd "$install_dir"
     ynh_use_ruby
     "$ynh_gem" update --system --no-document
-    ynh_exec_as "$app" "$ynh_ruby_load_path" "$ynh_gem" install bundler passenger --no-document
+    "$ynh_gem" install bundler passenger --no-document
     ynh_exec_as "$app" "$ynh_ruby_load_path" bundle config set --local without 'development test rmagick'
     ynh_exec_as "$app" "$ynh_ruby_load_path" bundle add 'webrick' --version '~> 1.7'
     ynh_exec_as "$app" "$ynh_ruby_load_path" bundle install
