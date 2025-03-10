3f33c7c849085f3db49bf7b689cc4aaf6223e26c
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-03-10 16:08:09 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,8 +10,8 @@ _redmine_ruby_install() {
     pushd "$install_dir"
         gem update --system --no-document
         gem install bundler passenger --no-document
-        bundle config set --local without 'development test rmagick'
-        bundle add 'webrick' --version '~> 1.7'
-        bundle install
+        ynh_exec_as_app bundle config set --local without 'development test rmagick'
+        ynh_exec_as_app bundle add 'webrick' --version '~> 1.7'
+        ynh_exec_as_app bundle install
     popd
 }
