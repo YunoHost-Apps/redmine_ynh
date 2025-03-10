734d6d68ce52117d4ce4591482a664a075923868
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-03-10 15:44:17 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,11 +8,10 @@ ruby_version="3.1"
 
 _redmine_ruby_install() {
     pushd "$install_dir"
-
-    gem update --system --no-document
-    gem install bundler passenger --no-document
-    bundle config set --local without 'development test rmagick'
-    bundle add 'webrick' --version '~> 1.7'
-    bundle install
+        gem update --system --no-document
+        gem install bundler passenger --no-document
+        bundle config set --local without 'development test rmagick'
+        bundle add 'webrick' --version '~> 1.7'
+        bundle install
     popd
 }
