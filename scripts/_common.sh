f9b06ea50083754ef687409e68de48ebe549055a
Kozmotronik <kozmotronik.developer@gmail.com>
2025-12-13 08:39:29 +0300

    Fix rubygem incompatibility error at installation

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 _redmine_ruby_install() {
     pushd "$install_dir"
-        gem update --system --no-document
+        gem update --system 3.4.7 --no-document
         gem install bundler passenger --no-document
         bundle config set --local without 'development test rmagick'
         bundle add 'webrick' --version '~> 1.7'
